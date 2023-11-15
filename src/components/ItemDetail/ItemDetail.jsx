51873ef09113194818eccd7d946a86d878cd8619
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2023-11-15 16:01:55 -0600

    ItemDetail Add

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -3,24 +3,16 @@ import ItemCount from "./ItemCount"
 const ItemDetail = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-100">
-      <div
-        className="flex flex-col p-6 m-3 space-y-10 bg-white rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16"
-      >
+      <div className="flex flex-col p-6 m-3 space-y-10 bg-white rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16" >
         <div>
-          <img
-            src="images/headphone.png"
-            alt=""
-            className="mx-auto duration-200 w-60 hover:scale-105"
-          />
+          <img src="images/headphone.png" alt="" className="mx-auto duration-200 w-60 hover:scale-105"/>
         </div>
 
         <div className="flex flex-col space-y-6">
           <div className="flex flex-col mb-4 space-y-3 text-center md:text-left">
             <div>
-              <div
-                className="inline-block px-3 py-1 text-sm text-white bg-black rounded-full"
-              >
-                Free Shipping
+              <div className="inline-block px-3 py-1 text-sm text-white bg-black rounded-full">
+                Envio Gratis
               </div>
             </div>
 
@@ -28,40 +20,23 @@ const ItemDetail = () => {
               Razer Kraken Kitty Edt Gaming Headset Quartz
             </div>
             <div className="flex flex-col mb-4 space-y-3 text-center md:text-left">
-              <p className="line-through">$799</p>
               <p className="text-5xl font-bold">$599</p>
               <p className="text-sm font-light text-gray-400">
                 This offer is valid until April 3rd or as long as stock lasts!
               </p>
             </div>
-
             <div className="group">
-              <button
-                className="w-full transition-all duration-150 bg-blue-700 text-white border-b-8 border-b-blue-700 rounded-lg group-hover:border-t-8 group-hover:border-b-0 group-hover:bg-blue-700 group-hover:border-t-blue-700 group-hover:shadow-lg">
-                <div
-                  className="px-8 py-4 duration-150 bg-blue-500 rounded-lg group-hover:bg-blue-700">
-                  Add to cart
-                </div>
-              </button>
+                <ItemCount/>
             </div>
-
             <div className="flex items-center space-x-3 group">
               <div
                 className="w-3 h-3 bg-green-400 rounded-full group-hover:animate-ping"></div>
-              <div className="text-sm">50+ pcs. in stock</div>
+              <div className="text-sm">50+ articulos en inventario</div>
             </div>
 
-            <div
-              className="flex flex-col space-y-4 md:space-y-0 md:space-x-4 md:flex-row">
-              <button
-                className="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150">
-                <img src="images/weight.png" alt="" className="w-8" />
-                <span>Add to cart</span>
-              </button>
-
-              <button
-                className="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150">
-                <img src="images/heart.png" alt="" className="w-8" />
+            <div className="flex space-y-4 md:space-y-0 md:space-x-4 flex-col">
+              <button className="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150">
+              <ion-icon name="heart-outline" size="large"></ion-icon>
                 <span>Add to wishlist</span>
               </button>
             </div>
